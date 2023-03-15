24e527a99b415eeeae199b5ca83c8d2c24191299
Olivier M <olivier@loungeup.com>
2023-03-15 15:46:01 +0000

    [gp] Split Kubernetes Pod into multiple containers

--- src/Nats/NatsHandler.php
+++ src/Nats/NatsHandler.php
@@ -22,13 +22,50 @@ class NatsHandler
     public function subscribeRoutes(array $routes, MessageDriver $messageDriver)
     {
         foreach ($routes as $route) {
-            $this->subscribeRoute($route, $messageDriver);
+            if (function_exists("config") && ($queue = config("app.name"))) {
+                $this->subscribeQueueRoute($route, $messageDriver, $queue);
+            } else {
+                $this->subscribeRoute($route, $messageDriver);
+            }
         }
     }
 
     public function subscribeRoute(Route $route, MessageDriver $messageDriver)
     {
         $this->client->subscribe($route->getEvent(), function (Message $message) use ($route, $messageDriver) {
+            if ($this->verbose) {
+                Log::debug(
+                    "HIT: " .
+                        $route->getEventRoute() .
+                        " | SUBJECT: " .
+                        $message->subject .
+                        " | PAYLOAD: " .
+                        $message->data,
+                );
+            }
+
+            $output = $messageDriver->handle($message, $route);
+
+            if ($this->verbose) {
+                Log::debug("REPLY : " . $output);
+            }
+
+            if ($message->reply && is_string($output)) {
+                $message->respond($output);
+            }
+        });
+
+        if ($this->verbose) {
+            Log::debug("Subscribe: " . $route->getEventRoute());
+        }
+    }
+
+    public function subscribeQueueRoute(Route $route, MessageDriver $messageDriver, string $queue)
+    {
+        $this->client->queueSubscribe($route->getEvent(), $queue, function (Message $message) use (
+            $route,
+            $messageDriver,
+        ) {
             if ($this->verbose) {
                 Log::debug(
                     "HIT: " .
